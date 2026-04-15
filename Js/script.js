e6dd2f57e66fbd12eb457e67b2301bceb1eff257
Vinicius Ferraroni <viniciusferraroni@gmail.com>
2026-04-15 16:50:43 -0300

    Atualizando tela de fundo

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ var time = document.getElementById('hours');
 var weeks = document.getElementById('day');
 
 function clock() {
-    // Clock
+    // Relógio
     var currentTime = new Date();
 
     var displayhours = currentTime.getHours();
@@ -22,7 +22,7 @@ function clock() {
 
     time.innerHTML = displayhours + ":" + displayminutes + ":" + displayseconds;
 
-    // Day
+    // Dia
     var displayday = currentTime.getDay();
 
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -34,6 +34,7 @@ function clock() {
 
 setInterval(clock, 100);
 
+// Clima
 function BuscarClima(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Sao%20Paulo,br&units=metric&lang=pt_br&appid=2f0769ba6488ad6f089c3481b881684d')
         .then(c => c.json())
@@ -51,3 +52,30 @@ function BuscarClima(){
 BuscarClima();
 setInterval(BuscarClima, 600000);
 
+// Estrelas
+function criarEstrelas() {
+    const vazio = document.getElementById('vazio-infinito');
+    const quantidade = 100;
+
+    for (let i = 0; i < quantidade; i++){
+        let estrela = document.createElement('div');
+        estrela.className = 'particula';
+
+        let tamanho = Math.random() * 3 + 'px';
+        estrela.style.width = tamanho;
+        estrela.style.height = tamanho;
+
+        estrela.style.top = '50%';
+        estrela.style.left = '50%';
+
+        estrela.style.background = Math.random() > 0.5 ? '#ffffff' : '#7000da';
+
+        estrela.style.setProperty('--x', (Math.random() * 2000 - 1000) + 'px');
+        estrela.style.setProperty('--y', (Math.random() * 2000 - 1000) + 'px');
+
+        estrela.style.animationDelay = (Math.random() * -20) + 's';
+
+        vazio.appendChild(estrela);
+    }
+}
+criarEstrelas();
